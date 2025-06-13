90f61bff60bfd024287b641e5bfd6cf117edb61b
Budankov Kostya <107200706+Budankov@users.noreply.github.com>
2025-06-13 21:39:32 +0300

    Update Camera components and CreateScreen

--- app/(tabs)/create.tsx
+++ app/(tabs)/create.tsx
@@ -1,6 +1,9 @@
 import CameraComponent from "@/components/Camera/Camera";
 import { useState } from "react";
-import { Button, Image, StyleSheet, View } from "react-native";
+import { Image, StyleSheet, Text, TouchableOpacity, View } from "react-native";
+
+// Icons
+import AntDesign from "@expo/vector-icons/AntDesign";
 
 export default function CreateScreen() {
   const [cameraOpen, setCameraOpen] = useState(true);
@@ -27,9 +30,30 @@ export default function CreateScreen() {
 
   return (
     <View style={styles.container}>
-      {photoUri && <Image source={{ uri: photoUri }} style={styles.preview} />}
-      <Button title="Зробити нове фото" onPress={() => setCameraOpen(true)} />
-      <Button title="Опублікувати" onPress={handlePublish} />
+      <View style={styles.previewContainer}>
+        {photoUri && (
+          <Image
+            resizeMode="cover"
+            source={{ uri: photoUri }}
+            style={styles.preview}
+          />
+        )}
+        <TouchableOpacity
+          style={styles.deletePhoto}
+          onPress={() => setCameraOpen(true)}
+        >
+          <AntDesign name="delete" size={30} color="white" />
+        </TouchableOpacity>
+      </View>
+      <View style={styles.submitContainer}>
+        <TouchableOpacity
+          style={styles.submitBtn}
+          activeOpacity={0.8}
+          onPress={handlePublish}
+        >
+          <Text style={styles.submitBtnText}>Опублікувати</Text>
+        </TouchableOpacity>
+      </View>
     </View>
   );
 }
@@ -37,12 +61,39 @@ export default function CreateScreen() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    justifyContent: "center",
-    alignItems: "center",
+    marginHorizontal: 20,
+  },
+  previewContainer: {
+    height: "50%",
+    position: "relative",
+  },
+  submitContainer: {
+    height: "50%",
   },
   preview: {
-    width: 200,
-    height: 300,
+    width: "100%",
+    height: "100%",
     marginBottom: 20,
+    borderRadius: 10,
+  },
+  deletePhoto: {
+    position: "absolute",
+    bottom: 10,
+    right: 10,
+  },
+  submitBtn: {
+    backgroundColor: "#FF6C00",
+    borderRadius: 100,
+    justifyContent: "center",
+    alignItems: "center",
+    marginTop: 22,
+  },
+  submitBtnText: {
+    color: "#FFF",
+    fontFamily: "Roboto",
+    fontWeight: "400",
+    fontSize: 16,
+    paddingTop: 16,
+    paddingBottom: 16,
   },
 });
--- components/Camera/Camera.tsx
+++ components/Camera/Camera.tsx
@@ -1,17 +1,30 @@
 import { CameraType, CameraView, useCameraPermissions } from "expo-camera";
-import { Camera } from "expo-camera/next"; // додай це, бо потрібен реальний Camera ref
+import { Camera } from "expo-camera/next";
+import { useRouter } from "expo-router";
 import { useRef, useState } from "react";
 import { Button, StyleSheet, Text, TouchableOpacity, View } from "react-native";
+
+// Icons
+import AntDesign from "@expo/vector-icons/AntDesign";
+import Entypo from "@expo/vector-icons/Entypo";
+import Ionicons from "@expo/vector-icons/Ionicons";
 
 type Props = {
   onPhotoTaken: (uri: string) => void;
   onClose: () => void;
 };
 
-export default function CameraComponent({ onPhotoTaken, onClose }: Props) {
+export default function CameraComponent({
+  onPhotoTaken,
+}: Omit<Props, "onClose">) {
   const [facing, setFacing] = useState<CameraType>("back");
   const [permission, requestPermission] = useCameraPermissions();
   const cameraRef = useRef<Camera>(null);
+  const router = useRouter();
+
+  const handleClose = () => {
+    router.replace("/(tabs)/home");
+  };
 
   if (!permission) return <View />;
   if (!permission.granted) {
@@ -40,15 +53,19 @@ export default function CameraComponent({ onPhotoTaken, onClose }: Props) {
     <View style={styles.container}>
       <CameraView ref={cameraRef} style={styles.camera} facing={facing}>
         <View style={styles.buttonContainer}>
-          <TouchableOpacity onPress={toggleCameraFacing}>
-            <Text style={styles.text}>Flip</Text>
-          </TouchableOpacity>
-          <TouchableOpacity onPress={takePhoto}>
-            <Text style={styles.text}>📸</Text>
-          </TouchableOpacity>
-          <TouchableOpacity onPress={onClose}>
-            <Text style={styles.text}>Закрити</Text>
-          </TouchableOpacity>
+          <View style={styles.buttonTopContainer}>
+            <TouchableOpacity onPress={toggleCameraFacing}>
+              <Ionicons name="camera-reverse-outline" size={40} color="white" />
+            </TouchableOpacity>
+            <TouchableOpacity onPress={handleClose}>
+              <AntDesign name="close" size={40} color="white" />
+            </TouchableOpacity>
+          </View>
+          <View style={styles.buttonBottomContainer}>
+            <TouchableOpacity style={styles.buttonSnap} onPress={takePhoto}>
+              <Entypo name="circle" size={50} color="black" />
+            </TouchableOpacity>
+          </View>
         </View>
       </CameraView>
     </View>
@@ -69,9 +86,28 @@ const styles = StyleSheet.create({
   },
   buttonContainer: {
     flex: 1,
+  },
+  buttonTopContainer: {
+    flex: 1,
     flexDirection: "row",
-    backgroundColor: "transparent",
-    margin: 64,
+    justifyContent: "space-between",
+    paddingHorizontal: 20,
+    paddingTop: 20,
+  },
+
+  buttonBottomContainer: {
+    flex: 1,
+    flexDirection: "column-reverse",
+    alignItems: "center",
+    paddingBottom: 20,
+  },
+  buttonSnap: {
+    justifyContent: "center",
+    alignItems: "center",
+    width: 60,
+    height: 60,
+    borderRadius: 50,
+    backgroundColor: "#fff",
   },
   button: {
     flex: 1,
