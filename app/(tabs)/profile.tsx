5d0689495cfafe1e5601100c7ee78723912213c7
Budankov Kostya <107200706+Budankov@users.noreply.github.com>
2025-07-03 20:56:09 +0300

    Update ProfileScreen

--- app/(tabs)/profile.tsx
+++ app/(tabs)/profile.tsx
@@ -1,9 +1,56 @@
-import { StyleSheet, Text, View } from "react-native";
+import { db } from "@/firebase/firebaseConfig";
+import { useAppSelector } from "@/redux/hooks";
+import { useFocusEffect } from "expo-router";
+import { collection, getDocs, query, where } from "firebase/firestore";
+import { useCallback, useState } from "react";
+import { Dimensions, FlatList, Image, StyleSheet, View } from "react-native";
+
+const screenWidth = Dimensions.get("window").width;
+const imageSize = screenWidth / 3 - 4;
 
 export default function ProfileScreen() {
+  const [posts, setPosts] = useState<any[]>([]);
+  const { user } = useAppSelector((state) => state.auth);
+
+  useFocusEffect(
+    useCallback(() => {
+      const fetchUserPosts = async () => {
+        if (!user?.uid) return;
+
+        try {
+          const q = query(
+            collection(db, "posts"),
+            where("uid", "==", user.uid)
+          );
+          const snapshot = await getDocs(q);
+
+          const userPosts = snapshot.docs.map((doc) => ({
+            id: doc.id,
+            ...doc.data(),
+          }));
+
+          setPosts(userPosts);
+        } catch (error) {
+          console.error("Помилка при отриманні постів користувача:", error);
+        }
+      };
+
+      fetchUserPosts();
+    }, [user?.uid])
+  );
+
   return (
     <View style={styles.container}>
-      <Text style={styles.text}>Profile Screen</Text>
+      <FlatList
+        data={posts}
+        keyExtractor={(item) => item.id}
+        numColumns={3}
+        contentContainerStyle={styles.grid}
+        columnWrapperStyle={styles.row}
+        renderItem={({ item }) => (
+          <Image source={{ uri: item.photoURL }} style={styles.image} />
+        )}
+      />
     </View>
   );
 }
@@ -11,10 +58,17 @@ export default function ProfileScreen() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    // backgroundColor: "#cb6565",
+    backgroundColor: "#fff",
   },
-  text: {
-    fontSize: 20,
-    color: "#000000",
+  grid: {
+    padding: 2,
+  },
+  row: {
+    justifyContent: "space-between",
+    marginBottom: 4,
+  },
+  image: {
+    width: imageSize,
+    height: imageSize,
   },
 });
